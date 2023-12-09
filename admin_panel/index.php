96a759155a17c935f8956da17e34a6fc4e566f32
Ahmedismail <144509690+AHMEDSH72@users.noreply.github.com>
2023-12-09 21:18:37 +0200

    Add files via upload

---
+++ admin_panel/index.php
@@ -0,0 +1,10 @@
+    <?php  include("components/header.php") ?>
+
+ <body class="sb-nav-fixed">
+    <?php  include("components/nav.php") ?>
+    <?php  include("components/aside_nav.php") ?>
+    <?php  include("components/footer.php") ?>
+    <?php  include("components/footer.php") ?>
+    <?php  include("components/content.php") ?>
+
+ </body>
